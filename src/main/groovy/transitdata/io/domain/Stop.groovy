7c570ac19e564a07ef2529dcde00836d8559831c
Andy Rudge <arudge@gmail.com>
2014-10-12 19:36:00 -0500

    H2 GTFS schema. Add the rest of Stops attributes
    Still need to add Postgres GTFS schema.

--- src/main/groovy/transitdata/io/domain/Stop.groovy
+++ src/main/groovy/transitdata/io/domain/Stop.groovy
@@ -8,18 +8,24 @@ import groovy.transform.ToString
 @EqualsAndHashCode
 @CompileStatic
 class Stop {
-  String id
-  String name
-  String desc
-  String lat
-  String lon
-  String street
-  String city
-  String region
-  String postcode
-  String country
-  String zoneid
-  String wheelchairboarding
-  String url
+    String id
+    String transit_system
+    String stop_id
+    String stop_code
+    String stop_name
+    String stop_desc
+    String stop_lat
+    String stop_lon
+    String stop_street
+    String stop_city
+    String stop_region
+    String stop_postcode
+    String stop_country
+    String zone_id
+    String stop_url
+    String location_type
+    String parent_station
+    String stop_timezone
+    String wheelchair_boarding
 }
 
--- src/test/groovy/transitdata/io/StopJobIntegrationSpec.groovy
+++ src/test/groovy/transitdata/io/StopJobIntegrationSpec.groovy
@@ -1,13 +1,16 @@
 package transitdata.io
 
+import com.jolbox.bonecp.BoneCPDataSource
 import org.springframework.batch.core.ExitStatus
 import org.springframework.batch.test.JobLauncherTestUtils
 import org.springframework.batch.test.StepScopeTestExecutionListener
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.jdbc.core.JdbcTemplate
 import org.springframework.test.context.ContextConfiguration
 import org.springframework.test.context.TestExecutionListeners
 import org.springframework.test.context.support.DependencyInjectionTestExecutionListener
 import spock.lang.Specification
+import transitdata.io.domain.Stop
 
 @ContextConfiguration('/test-config.xml')
 @TestExecutionListeners([
@@ -16,22 +19,47 @@ import spock.lang.Specification
 ])
 class StopJobIntegrationSpec extends Specification {
 
-  @Autowired
-  JobLauncherTestUtils jobLauncher
+    @Autowired
+    JobLauncherTestUtils jobLauncher
 
-  void 'simple properties injection'() {
+    @Autowired
+    BoneCPDataSource dataSource
 
-    expect:
-    jobLauncher != null
-  }
+    JdbcTemplate jdbcTemplate
 
-  void 'launch job'() {
+    def setup() {
+        jdbcTemplate = new JdbcTemplate(dataSource)
+    }
 
-    when: 'job is run'
-    def jobExecution = jobLauncher.launchJob()
+    void 'simple properties injection'() {
+        expect:
+        jobLauncher != null
+    }
 
-    then: 'it completes successfully (execution is synchronous, so we don\'t have to wait for completion)'
-    jobExecution.exitStatus == ExitStatus.COMPLETED
-  }
+    void 'launch job'() {
+        given:
+        Stop expectedStop = new Stop(
+                stop_id: '1000',
+                stop_name: '50th St W & Upton Ave S',
+                stop_desc: 'Near side E',
+                stop_lat: 44.912365,
+                stop_lon: '-93.315178',
+                stop_street: '50th St W',
+                stop_city: 'MINNEAPOLIS'
+        )
+
+        when: 'job is run'
+        def jobExecution = jobLauncher.launchJob()
+        List<Stop> stopList = jdbcTemplate.queryForList('select * from stop')
+
+        then: 'it completes successfully (execution is synchronous, so we don\'t have to wait for completion)'
+        jobExecution.exitStatus == ExitStatus.COMPLETED
+
+        and:
+        stopList.size() == 7
+        stopList.first().stop_id == expectedStop.stop_id
+        stopList.first().stop_name == expectedStop.stop_name
+        stopList.first().stop_city == expectedStop.stop_city
+    }
 
 }
