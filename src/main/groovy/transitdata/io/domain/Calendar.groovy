4f704889da103e63f2131a2ff85f6d94d5c6210a
Steven McAdams <steven.mcadams@target.com>
2014-10-23 19:43:11 -0500

    configuration cleanup for postgres database execution

--- src/main/groovy/transitdata/io/domain/Calendar.groovy
+++ src/main/groovy/transitdata/io/domain/Calendar.groovy
@@ -3,13 +3,13 @@ package transitdata.io.domain
 class Calendar {
 
     String service_id
-    String monday
-    String tuesday
-    String wednesday
-    String thursday
-    String friday
-    String saturday
-    String sunday
+    Integer monday
+    Integer tuesday
+    Integer wednesday
+    Integer thursday
+    Integer friday
+    Integer saturday
+    Integer sunday
     String start_date
     String end_date
 }
